bd62f38f12e341b493ec64d2fe7d9fc4139cdea7
Garrett Myrick <garrett@Garretts-Pro.netgear.com>
2015-03-04 22:31:42 -0800

    Added test class.

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Garrett Myrick on 3/4/15.
+//  Copyright (c) 2015 Garrett Myrick. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Garrett Myrick on 3/4/15.
+//  Copyright (c) 2015 Garrett Myrick. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
