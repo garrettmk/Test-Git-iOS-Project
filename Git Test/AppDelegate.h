e99483529a044345c0112c167d3c35b22ca23344
Garrett Myrick <garrett@Garretts-Pro.netgear.com>
2015-03-04 21:11:34 -0800

    Initial Commit

---
+++ Git Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by Garrett Myrick on 3/4/15.
+//  Copyright (c) 2015 Garrett Myrick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
